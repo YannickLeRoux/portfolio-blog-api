608ccf5766b639da8437e6ae085db7a812905caf
Yannick Le Roux <yannick.leroux971@gmail.com>
2020-03-28 21:37:41 -0700

    push to heroku

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+const rateLimit = require('express-rate-limit');
+const { body, check } = require('express-validator');
 const { pool } = require('./config');
 
 const app = express();
@@ -8,9 +12,31 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(compression());
+app.use(helmet());
 
-const getBooks = (request, response) => {
-  pool.query('SELECT * FROM posts', (error, results) => {
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5 // 5 requests,
+});
+
+app.use(limiter);
+
+// const postLimiter = rateLimit({
+//   windowMs: 1 * 60 * 1000,
+//   max: 1,
+// })
+
+// for prod
+// const isProduction = process.env.NODE_ENV === 'production'
+// const origin = {
+//   origin: isProduction ? 'https://www.example.com' : '*',
+// }
+
+// app.use(cors(origin))
+
+const getPosts = (request, response) => {
+  pool.query('SELECT * FROM posts ORDER BY created_at ASC', (error, results) => {
     if (error) {
       throw error;
     }
@@ -18,25 +44,79 @@ const getBooks = (request, response) => {
   });
 };
 
-// const addBook = (request, response) => {
-//   const { author, title } = request.body;
+const getPost = (request, response) => {
+  const id = parseInt(request.params.id);
 
-//   pool.query('INSERT INTO posts (author, title) VALUES ($1, $2)', [author, title], error => {
-//     if (error) {
-//       throw error;
-//     }
-//     response.status(201).json({ status: 'success', message: 'Book added.' });
-//   });
-// };
+  pool.query('SELECT * FROM posts WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const addPost = (request, response) => {
+  const { title, tags, subtitle, content } = request.body;
+  const created_at = Date(Date.now());
+  const slug = title
+    .trim()
+    .toLowerCase()
+    .replace(/\s/g, '-');
+
+  pool.query(
+    'INSERT INTO posts (title, slug, tags, subtitle, content, created_at) VALUES ($1, $2, $3, $4, $5, $6)',
+    [title, slug, tags, subtitle, content, created_at],
+    error => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).json({ status: 'success', message: response.body });
+    }
+  );
+};
+
+const deletePost = (request, response) => {
+  const id = parseInt(request.params.id);
+  pool.query('DELETE FROM posts WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`Post deleted with ID: ${id}`);
+  });
+};
+
+const updatePost = (request, response) => {
+  const id = parseInt(request.params.id);
+  const { title, tags, subtitle, content } = request.body;
+  const updated_at = Date(Date.now());
+
+  pool.query(
+    'UPDATE posts SET title = $1, tags = $2, subtitle = $3, content = $4 updated_at = $5 WHERE id = $6',
+    [title, tags, subtitle, content, updated_at, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`Post modified with ID: ${id}`);
+    }
+  );
+};
 
 app
   .route('/posts')
   // GET endpoint
-  .get(getBooks);
-// POST endpoint
-// .post(addBook);
+  .get(getPosts)
+  // POST endpoint
+  .post(addPost);
+
+app
+  .route('/posts/:id')
+  .get(getPost)
+  .put(updatePost)
+  .delete(deletePost);
 
 // Start server
+const port = process.env.PORT || 3002;
 app.listen(process.env.PORT || 3002, () => {
-  console.log(`Server listening...`);
+  console.log(`Server listening on port ${port}...`);
 });
--- init.sql
+++ init.sql
@@ -1,13 +1,16 @@
-CREATE TABLE posts (
+CREATE TABLE posts
+(
   ID SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   slug VARCHAR(255) NOT NULL,
-  tags VARCHAR(50)[],
+  tags VARCHAR(255),
   subtitle VARCHAR(255),
   content TEXT NOT NULL,
   created_at VARCHAR(255) NOT NULL,
   updated_at VARCHAR(255)
 );
 
-INSERT INTO posts (title, subtitle, content, created_at, slug, tags)
-VALUES  ('Test title 1','Subtitle 12','jsjhgjkhdfkgj','12/12/2009', 'this-slug', 'tag1,tag2');
+INSERT INTO posts
+  (title, subtitle, content, created_at, slug, tags)
+VALUES
+  ('Test title 1', 'Subtitle 12', 'jsjhgjkhdfkgj', '12/12/2009', 'this-slug', 'tag1,tag2');
